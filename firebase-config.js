930ee0dfc088c0c3d8d2f8b0332d5baeb4748aad
fundacionsocial-debug <fundacionsocial@gimnasioemocionalmb.com>
2026-05-07 15:02:11 -0500

    Conectar app con Firebase Web Config

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Configuracion publica de Firebase Web App para Amitabha Luz Infinita.
+// Esta informacion no es una contrasena privada; la seguridad real se controla con Firebase Authentication y Firestore Rules.
+window.AMITABHA_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCN4NaH95Nv9lhDE9IRKirgDpBBYlEcJOg",
+  authDomain: "amitabha-luz-infinita.firebaseapp.com",
+  projectId: "amitabha-luz-infinita",
+  storageBucket: "amitabha-luz-infinita.firebasestorage.app",
+  messagingSenderId: "235871345501",
+  appId: "1:235871345501:web:c0cfabf9b49d2628a8e6b4",
+  measurementId: "G-EGZZ67EEE2"
+};
